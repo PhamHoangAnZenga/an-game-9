4ae043e02560119131e6151527d12f299fab59b0
KNSBA <phamhoangan@zenga.com.vn>
2026-08-20 17:06:13 +0700

    add button, remove debug

--- SafeArea/Assets/Project/Scripts/SafeArea.cs
+++ SafeArea/Assets/Project/Scripts/SafeArea.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class SafeArea : MonoBehaviour
@@ -7,33 +6,51 @@ public class SafeArea : MonoBehaviour
 
     [SerializeField] RectTransform _topUnsafe;
     [SerializeField] RectTransform _bottomUnsafe;
+    [SerializeField] RectTransform _leftUnsafe;
+    [SerializeField] RectTransform _rightUnsafe;
 
-    public void ShowInfo()
+    void Start()
     {
-        Debug.Log($"witdh: {Screen.width} , height: {Screen.height}");
-        Debug.Log(Screen.safeArea);
+        FixSafeArea();
     }
 
-    public void FixSafeArea()
+    void FixSafeArea()
     {
-        float height = Screen.height;
         float width = Screen.width;
-        float safeHeight = Screen.safeArea.x;
-        float safeWidth = Screen.safeArea.y;
+        float height = Screen.height;
 
-        Debug.Log(safeHeight / height + " " + safeWidth / width);
-        float heightRatio = safeHeight / height / 2;
-        float widthRatio = safeWidth / width / 2;
+        float safeLeft = Screen.safeArea.x;
+        float safeBottom = Screen.safeArea.y;
+        float safeRight = safeLeft + Screen.safeArea.width;
+        float safeTop = safeBottom + Screen.safeArea.height;
 
-        _rectTransform.anchorMax = new Vector2(1 - heightRatio, 1 - widthRatio);
-        _rectTransform.anchorMin = new Vector2(heightRatio, widthRatio);
+        float leftRatio = safeLeft / width;
+        float bottomRatio = safeBottom / height;
+        float rightRatio = (width - safeRight) / width;
+        float topRatio = (height - safeTop) / height;
 
-        SetUnsafeArea();
+        _rectTransform.anchorMin = new Vector2(leftRatio, bottomRatio);
+        _rectTransform.anchorMax = new Vector2(1 - rightRatio, 1 - topRatio);
+
+        SetUnsafeArea(leftRatio, bottomRatio, rightRatio, topRatio);
+
     }
 
-    void SetUnsafeArea()
+    void SetUnsafeArea(float leftRatio, float bottomRatio, float rightRatio, float topRatio)
     {
-        _topUnsafe = new Vector2();
+        Debug.Log(leftRatio + " " + bottomRatio + " " + rightRatio + " " + topRatio);
+        _leftUnsafe.anchorMax = new Vector2(leftRatio, 1);
+        _leftUnsafe.anchorMin = new Vector2(0, 0);
+
+        _bottomUnsafe.anchorMax = new Vector2(1, bottomRatio);
+        _bottomUnsafe.anchorMin = new Vector2(0, 0);
+
+        _rightUnsafe.anchorMax = new Vector2(1, 1);
+        _rightUnsafe.anchorMin = new Vector2(1 - rightRatio, 0);
+
+        _topUnsafe.anchorMax = new Vector2(1, 1);
+        _topUnsafe.anchorMin = new Vector2(0, 1 - topRatio);
+
     }
+
 }
- 
